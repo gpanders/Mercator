5abc8d3fa60ecfb8cd2c5241227092b85206be83
Greg Anders <greg@gpanders.com>
2017-08-28 17:38:08 -0500

    Update sources

--- sources.cmake
+++ sources.cmake
@@ -4,13 +4,15 @@
 
 set(
     SOURCES
+    ${PROJECT_SOURCE_DIR}/src/camera.h
     ${PROJECT_SOURCE_DIR}/src/mercator.h
     ${PROJECT_SOURCE_DIR}/src/point.h
+    ${PROJECT_SOURCE_DIR}/src/camera.cc
     ${PROJECT_SOURCE_DIR}/src/mercator.cc
     ${PROJECT_SOURCE_DIR}/src/point.cc
     ${PROJECT_SOURCE_DIR}/src/util/colmap.h
-    ${PROJECT_SOURCE_DIR}/src/util/types.h
     ${PROJECT_SOURCE_DIR}/src/util/config.h
-    ${PROJECT_SOURCE_DIR}/src/util/config.cc
+    ${PROJECT_SOURCE_DIR}/src/util/types.h
     ${PROJECT_SOURCE_DIR}/src/util/colmap.cc
+    ${PROJECT_SOURCE_DIR}/src/util/config.cc
 )

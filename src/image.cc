2a3469e2d9c766523822129a73961aa8016a5ae5
Greg Anders <greg@gpanders.com>
2017-08-29 11:00:31 -0500

    Add Image class

---
+++ src/image.cc
@@ -0,0 +1,56 @@
+#include "image.h"
+
+namespace mercator {
+
+Image::Image()
+  : image_id_(-1), camera_id_(-1), num_points3d_(0) {}
+
+uint32_t Image::ImageId() const { return image_id_; }
+
+void Image::SetImageId(const uint32_t image_id) { image_id_ = image_id; }
+
+const std::string& Image::Name() const { return name_; }
+
+std::string& Image::Name() { return name_; }
+
+void Image::SetName(const std::string& name) { name_ = name; }
+
+uint32_t Image::CameraId() const { return camera_id_; }
+
+void Image::SetCameraId(const uint32_t camera_id) { camera_id_ = camera_id; }
+
+uint32_t Image::NumPoints3d() const { return num_points3d_; }
+
+void Image::SetNumPoints3d(const uint32_t num_points_3d) { num_points3d_ = num_points_3d; }
+
+const Eigen::Quaterniond& Image::Rotation() const { return rotation_; }
+
+Eigen::Quaterniond& Image::Rotation() { return rotation_; }
+
+void Image::SetRotation(const Eigen::Quaterniond& rotation) { rotation_ = rotation; }
+
+const Eigen::Vector3d& Image::Translation() const { return translation_; }
+
+Eigen::Vector3d& Image::Translation() { return translation_; }
+
+void Image::SetTranslation(const Eigen::Vector3d& translation) { translation_ = translation; }
+
+const std::vector<Point2d>& Image::Points2d() const { return points2d_; }
+
+std::vector<Point2d>& Image::Points2d() { return points2d_; }
+
+void Image::SetPoints2d(const std::vector<Point2d>& points2d) { points2d_ = points2d; }
+
+uint32_t Image::NumPoints2d() const { return points2d_.size(); }
+
+void Image::SetPoint3dForPoint2d(const uint32_t point2d_idx,
+                                 const uint64_t point3d_id)
+{
+  Point2d& point2d = points2d_.at(point2d_idx);
+  if (!point2d.HasPoint3d()) {
+    num_points3d_ += 1;
+  }
+  point2d.SetPoint3dId(point3d_id);
+}
+
+}
---
+++ src/image.h
@@ -0,0 +1,64 @@
+#ifndef MERCATOR_IMAGE_H_
+#define MERCATOR_IMAGE_H_
+
+#include <vector>
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+#include "point2d.h"
+
+namespace mercator {
+
+class Image {
+ public:
+  Image();
+
+  uint32_t ImageId() const;
+  void SetImageId(const uint32_t image_id);
+
+  const std::string& Name() const;
+  std::string& Name();
+  void SetName(const std::string& name);
+
+  uint32_t CameraId() const;
+  void SetCameraId(const uint32_t camera_id);
+
+  uint32_t NumPoints3d() const;
+  void SetNumPoints3d(const uint32_t num_points_3d);
+
+  const Eigen::Quaterniond& Rotation() const;
+  Eigen::Quaterniond& Rotation();
+  void SetRotation(const Eigen::Quaterniond& rotation);
+
+  const Eigen::Vector3d& Translation() const;
+  Eigen::Vector3d& Translation();
+  void SetTranslation(const Eigen::Vector3d& translation);
+
+  const std::vector<Point2d>& Points2d() const;
+  std::vector<Point2d>& Points2d();
+  void SetPoints2d(const std::vector<Point2d>& points2d);
+  uint32_t NumPoints2d() const;
+
+  void SetPoint3dForPoint2d(const uint32_t point2d_idx,
+                            const uint64_t point3d_id);
+
+ private:
+  uint32_t image_id_;
+
+  std::string name_;
+
+  uint32_t camera_id_;
+
+  uint32_t num_points3d_;
+
+  Eigen::Quaterniond rotation_;
+
+  Eigen::Vector3d translation_;
+
+  std::vector<Point2d> points2d_;
+};
+
+} // namespace mercator
+
+#endif // MERCATOR_IMAGE_H_

ee036341ffa5d91b40df94f1ee810dfc768eec18
Greg Anders <greg@gpanders.com>
2017-08-29 11:00:38 -0500

    Add Point2d class

---
+++ src/point2d.cc
@@ -0,0 +1,38 @@
+#include "point2d.h"
+
+namespace mercator {
+
+Point2d::Point2d()
+  : coords_(Eigen::Vector2d::Zero()), point3d_id_(-1) {}
+
+Point2d::Point2d(const double x, const double y)
+  : coords_(x, y), point3d_id_(-1) {}
+
+const Eigen::Vector2d& Point2d::Coords() const { return coords_; }
+
+Eigen::Vector2d& Point2d::Coords() { return coords_; }
+
+double Point2d::Coords(const size_t idx) const { return coords_(idx); }
+
+double& Point2d::Coords(const size_t idx) { return coords_(idx); }
+
+void SetPoint2d::Coords(const Eigen::Vector2d& coords) { coords_ = coords; }
+
+double Point2d::X() const { return coords_(0); }
+
+double& Point2d::X() { return coords_(0); }
+
+double Point2d::Y() const { return coords_(1); }
+
+double& Point2d::Y() { return coords_(1); }
+
+uint64_t Point2d::Point3dId() const { return point3d_id_; }
+
+bool Point2d::HasPoint3d() const { return point3d_id_ != -1; }
+
+void Point2d::SetPoint3dId(const uint64_t point3d_id)
+{
+  point3d_id_ = point3d_id;
+}
+
+}
---
+++ src/point2d.h
@@ -0,0 +1,38 @@
+#ifndef MERCATOR_POINT2D_H_
+#define MERCATOR_POINT2D_H_
+
+#include <Eigen/Core>
+
+namespace mercator {
+
+class Point2d {
+ public:
+  Point2d();
+  Point2d(const double x, const double y);
+
+  const Eigen::Vector2d& Coords() const;
+  Eigen::Vector2d& Coords();
+  double Coords(const size_t idx) const;
+  double& Coords(const size_t idx);
+  void SetCoords(const Eigen::Vector2d& coords);
+
+  double X() const;
+  double& X();
+
+  double Y() const;
+  double& Y();
+
+  uint64_t Point3dId() const;
+  bool HasPoint3d() const;
+  void SetPoint3dId(const uint64_t point3d_id);
+
+ private:
+  Eigen::Vector2d coords_;
+
+  uint64_t point3d_id_;
+
+};
+
+}
+
+#endif // MERCATOR_POINT2D_H_

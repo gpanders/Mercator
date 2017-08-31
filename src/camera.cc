5175815e8dd4b4471709efb97691b5570ddeb0df
Greg Anders <ganders@utexas.edu>
2017-08-31 15:33:22 -0500

    Make WorldToImage static and move implementation to header file

--- src/camera.cc
+++ src/camera.cc
@@ -4,47 +4,34 @@ namespace mercator {
 
 Camera::Camera() : camera_id_(-1), width_(0), height_(0), params_(4, 0) {}
 
-const uint32_t Camera::CameraId() const { return camera_id_; }
+uint32_t Camera::CameraId() const { return camera_id_; }
+
+uint32_t& Camera::CameraId() { return camera_id_; }
 
 void Camera::SetCameraId(const uint32_t camera_id) { camera_id_ = camera_id; }
 
-const uint64_t Camera::Width() const { return width_; }
+uint64_t Camera::Width() const { return width_; }
+
+uint64_t& Camera::Width(){ return width_; }
 
 void Camera::SetWidth(const uint64_t width) { width_ = width; }
 
-const uint64_t Camera::Height() const { return height_; }
+uint64_t Camera::Height() const { return height_; }
+
+uint64_t& Camera::Height() { return height_; }
 
 void Camera::SetHeight(const uint64_t height) { height_ = height; }
+
+double Camera::PixelSize() const { return pixel_size_; }
+
+double& Camera::PixelSize() { return pixel_size_; }
+
+void Camera::SetPixelSize(const double pixel_size) { pixel_size_ = pixel_size; }
 
 const std::vector<double> Camera::Params() const { return params_; }
 
 std::vector<double>& Camera::Params() { return params_; }
 
 void Camera::SetParams(const std::vector<double>& params) { params_ = params; }
-
-void Camera::WorldToImage(const Eigen::Vector3d& world, Eigen::Vector2d* image)
-{
-  // Get params from params vector
-  // These could change based on the camera model being used
-  const double focal_length = params_[0];
-  const double cx = params_[1];
-  const double cy = params_[2];
-  const double radial_distortion = params_[3];
-
-  // Normalize to image plane
-  const double X = world(0) / world(2);
-  const double Y = world(1) / world(2);
-
-  // Calculate radial distortion
-  const double r2 = X*X + Y*Y;
-  const double radial = radial_distortion * r2;
-
-  // Calculate image coordinates relative
-  const double x = X * (1 + radial);
-  const double y = Y * (1 + radial);
-
-  (*image)(0) = focal_length * x + cx;
-  (*image)(1) = focal_length * y + cy;
-}
 
 } // namespace mercator
--- src/camera.h
+++ src/camera.h
@@ -11,28 +11,63 @@ class Camera {
  public:
   Camera();
 
-  const uint32_t CameraId() const;
+  uint32_t CameraId() const;
+  uint32_t& CameraId();
   void SetCameraId(const uint32_t camera_id);
 
-  const uint64_t Width() const;
+  uint64_t Width() const;
+  uint64_t& Width();
   void SetWidth(const uint64_t width);
 
-  const uint64_t Height() const;
+  uint64_t Height() const;
+  uint64_t& Height();
   void SetHeight(const uint64_t height);
+
+  double PixelSize() const;
+  double& PixelSize();
+  void SetPixelSize(const double pixel_size);
 
   const std::vector<double> Params() const;
   std::vector<double>& Params();
   void SetParams(const std::vector<double>& params);
 
-  void WorldToImage(const Eigen::Vector3d& world, Eigen::Vector2d* image);
+  template<typename T>
+  static void WorldToImage(const T* const params, const T* const world,
+                           T* image);
 
  private:
   uint32_t camera_id_;
-  uint64_t width_;
-  uint64_t height_;
+  uint64_t width_; // px
+  uint64_t height_; // px
+  double pixel_size_; // mm
+
+  // Focal length, principal point, radial distortion, etc.
   std::vector<double> params_;
 
 };
+
+template<typename T>
+void Camera::WorldToImage(const T* const params, const T* const world,
+                          T* image)
+{
+  // Get params from params vector
+  // These could change based on the camera model being used
+  const T& focal_length = params[0];
+  const T& cx = params[1];
+  const T& cy = params[2];
+  const T& radial_distortion = params[3];
+
+  // Normalize to image plane
+  const T xp = world[0] / world[2];
+  const T yp = world[1] / world[2];
+
+  // Calculate radial distortion
+  const T r2 = xp*xp + yp*yp;
+  const T distortion = 1.0 + r2 * radial_distortion;
+
+  image[0] = focal_length * distortion * xp + cx;
+  image[1] = focal_length * distortion * yp + cy;
+}
 
 } // namespace mercator
 

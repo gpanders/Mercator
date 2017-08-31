d602cc274cdf670a30029148f9ff38b3ccac1473
Greg Anders <ganders@utexas.edu>
2017-08-31 15:33:03 -0500

    Update sources

--- sources.cmake
+++ sources.cmake
@@ -4,18 +4,22 @@
 
 set(
     SOURCES
+    ${PROJECT_SOURCE_DIR}/src/mercator.h
     ${PROJECT_SOURCE_DIR}/src/camera.h
     ${PROJECT_SOURCE_DIR}/src/image.h
     ${PROJECT_SOURCE_DIR}/src/point2d.h
+    ${PROJECT_SOURCE_DIR}/src/cost_functions.h
+    ${PROJECT_SOURCE_DIR}/src/bundle_adjustment.h
     ${PROJECT_SOURCE_DIR}/src/point3d.h
     ${PROJECT_SOURCE_DIR}/src/camera.cc
-    ${PROJECT_SOURCE_DIR}/src/image.cc
     ${PROJECT_SOURCE_DIR}/src/mercator.cc
-    ${PROJECT_SOURCE_DIR}/src/point2d.cc
     ${PROJECT_SOURCE_DIR}/src/point3d.cc
+    ${PROJECT_SOURCE_DIR}/src/image.cc
+    ${PROJECT_SOURCE_DIR}/src/bundle_adjustment.cc
+    ${PROJECT_SOURCE_DIR}/src/point2d.cc
     ${PROJECT_SOURCE_DIR}/src/util/colmap.h
-    ${PROJECT_SOURCE_DIR}/src/util/config.h
     ${PROJECT_SOURCE_DIR}/src/util/types.h
-    ${PROJECT_SOURCE_DIR}/src/util/colmap.cc
+    ${PROJECT_SOURCE_DIR}/src/util/config.h
     ${PROJECT_SOURCE_DIR}/src/util/config.cc
+    ${PROJECT_SOURCE_DIR}/src/util/colmap.cc
 )

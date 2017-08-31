db45fecaa051d8706e1ad75d541bd9caae7556e9
Greg Anders <ganders@utexas.edu>
2017-08-31 15:36:43 -0500

    Add BundleAdjustment class

---
+++ src/bundle_adjustment.cc
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <string>
+
+#include "bundle_adjustment.h"
+
+namespace mercator {
+
+BundleAdjustment::BundleAdjustment(const BundleAdjustment::Options& options)
+  : options_(options) {}
+
+void BundleAdjustment::AddCamera(const Camera& camera)
+{
+  cameras_.emplace(camera.CameraId(), camera);
+}
+
+void BundleAdjustment::AddImage(const Image& image)
+{
+  images_.emplace(image.ImageId(), image);
+}
+
+void BundleAdjustment::AddPoint(const Point3d& point3d)
+{
+  points3d_.emplace(point3d.Point3dId(), point3d);
+  // problem_->AddResidualBlock(ReprojectionCostFunction::Create(
+  //       camera, q, T, point2d), loss_function_, point3d->Coords().data());
+}
+
+void BundleAdjustment::Run()
+{
+  problem_.reset();
+  summary_.reset();
+
+  for (const auto& image : images_)
+  {
+    const Camera& camera = cameras_.at(image.second.CameraId());
+    for (const auto& point2d : image.second.Points2d())
+    {
+      if (!point2d.HasPoint3d())
+      {
+        continue;
+      }
+
+      Point3d& point3d = points3d_.at(point2d.Point3dId());
+      problem_->AddResidualBlock(
+          ReprojectionCostFunction::Create(
+            camera,
+            image.second.Rotation(),
+            image.second.Translation(),
+            point2d
+          ),
+          options_.loss_function,
+          point3d.Coords().data());
+    }
+  }
+
+  if (problem_->NumResiduals() > 0)
+  {
+    ceres::Solver::Options solver_options = options_.solver_options; 
+    if (solver_options.num_threads == -1)
+    {
+#ifdef OPENMP_ENABLED
+      solver_options.num_threads = omp_get_max_threads();
+      solver_options.num_linear_solver_threads = omp_get_max_threads();
+#else
+      solver_options.num_threads = 1;
+      solver_options.num_linear_solver_threads = 1;
+#endif
+    }
+
+    ceres::Solve(solver_options, problem_.get(), summary_.get());
+  }
+
+}
+
+void BundleAdjustment::ComputeCovariance(std::vector<Point3d>* points)
+{
+  ceres::Covariance::Options covariance_options = options_.covariance_options;
+  if (covariance_options.num_threads == -1)
+  {
+#ifdef OPENMP_ENABLED
+    covariance_options.num_threads = omp_get_max_threads();
+#else
+    covariance_options.num_threads = 1;
+#endif
+  }
+  ceres::Covariance covariance(covariance_options);
+
+  std::vector< std::pair<const double*, const double*> > covariance_blocks;
+  for (const auto& point : *points)
+  {
+    const double* data = point.Coords().data();
+    if (problem_->HasParameterBlock(data))
+    {
+      covariance_blocks.emplace_back(data, data);
+    }
+  }
+
+  CHECK(covariance.Compute(covariance_blocks, problem_.get()));
+
+  for (auto& point : *points)
+  {
+    const double* data = point.Coords().data();
+    covariance.GetCovarianceBlock(data, data, point.Covariance().data());
+  }
+}
+
+void BundleAdjustment::PrintSummary(const bool print_full_report)
+{
+  const std::string report = print_full_report
+    ? summary_->FullReport() : summary_->BriefReport();
+  std::cout << report << std::endl;
+}
+
+const ceres::Solver::Summary BundleAdjustment::Summary() const
+{
+  return *summary_;
+}
+
+}
---
+++ src/bundle_adjustment.h
@@ -0,0 +1,77 @@
+#ifndef MERCATOR_BUNDLE_ADJUSTMENT_H_
+#define MERCATOR_BUNDLE_ADJUSTMENT_H_
+
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+#include <ceres/ceres.h>
+
+#include <Eigen/Core>
+
+#include "cost_functions.h"
+#include "point2d.h"
+#include "point3d.h"
+
+namespace mercator {
+
+class BundleAdjustment {
+ public:
+  struct Options {
+    // Type of loss function to use. If NULL, ceres will default to a squared
+    // loss function
+    ceres::LossFunction* loss_function = nullptr;
+
+    // Ceres specific options
+    ceres::Solver::Options solver_options;
+
+    ceres::Covariance::Options covariance_options;
+
+    // Whether or not to print the summary of the bundle adjustment after
+    // completion
+    bool print_summary = false;
+
+    // If print_summary is true, whether or not to print the FullReport or the
+    // BriefReport
+    bool print_full_report = false;
+
+    Options()
+    {
+      solver_options.linear_solver_type = ceres::DENSE_SCHUR;
+      solver_options.num_threads = -1;
+
+      covariance_options.num_threads = -1;
+      // covariance_options.algorithm_type = ceres::DENSE_SVD;
+      // covariance_options.null_space_rank = 1;
+    }
+  };
+
+  BundleAdjustment(const Options& options);
+
+  void AddCamera(const Camera& camera);
+
+  void AddImage(const Image& image);
+
+  void AddPoint(const Point3d& point3d);
+
+  void Run();
+
+  void ComputeCovariance(std::vector<Point3d>* points);
+
+  void PrintSummary(const bool print_full_report);
+
+  const ceres::Solver::Summary Summary() const;
+ private:
+  std::unique_ptr<ceres::Problem> problem_;
+  std::unique_ptr<ceres::Solver::Summary> summary_;
+
+  std::unordered_map<uint32_t, Camera> cameras_;
+  std::unordered_map<uint32_t, Image> images_;
+  std::unordered_map<uint64_t, Point3d> points3d_;
+
+  const Options options_;
+};
+
+}
+
+#endif // MERCATOR_BUNDLE_ADJUSTMENT_H_
